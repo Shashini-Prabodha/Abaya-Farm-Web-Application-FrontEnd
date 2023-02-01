b25f06b1a80d8134b0911db66ba2d39ef61e4e28
SHASHINI PRABODHA <shashiniprabodha0503@gmail.com>
2023-02-01 23:36:50 +0530

    create cage and order pages

--- assets/js/singleController.js
+++ assets/js/singleController.js
@@ -1,44 +1,56 @@
-
 $('#btnDash').on('click', function () {
     setVisibility('block', 'none', 'none', 'none', 'none', 'none', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnPurchase').on('click', function () {
     setVisibility('none', 'block', 'none', 'none', 'none', 'none', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnEditBatch').on('click', function () {
     setVisibility('none', 'none', 'block', 'none', 'none', 'none', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnMortality').on('click', function () {
     setVisibility('none', 'none', 'none', 'block', 'none', 'none', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnPurchaseFeed').on('click', function () {
     setVisibility('none', 'none', 'none', 'none', 'block', 'none', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnFeedConsumption').on('click', function () {
     setVisibility('none', 'none', 'none', 'none', 'none', 'block', 'none',
-        'none');
+        'none', 'none', 'none','none');
 });
 
 $('#btnFeedStockSummary').on('click', function () {
     setVisibility('none', 'none', 'none', 'none', 'none', 'none', 'block',
-        'none');
+        'none', 'none', 'none','none');
+});
+$('#btnCage').on('click', function () {
+    setVisibility('none', 'none', 'none', 'none', 'none', 'none', 'none',
+        'block', 'none', 'none','none');
+});
+$('#btnOrderDetails').on('click', function () {
+    setVisibility('none', 'none', 'none', 'none', 'none', 'none', 'none',
+        'none', 'block', 'none','none');
+});
+$('#btnPlaceOrder').on('click', function () {
+    setVisibility('none', 'none', 'none', 'none', 'none', 'none', 'none',
+        'none', 'none','block', 'none');
 });
 $('#btnProfile').on('click', function () {
     setVisibility('none', 'none', 'none', 'none', 'none', 'none', 'none',
-        'block');
+        'none', 'none', 'none','block');
 });
 
-function setVisibility(main, birdsComponent, birdsEditComponent, mortalityComponent, feedPurchaseComponent, feedConsumptionComponent, feedStockSummaryComoponent,profile) {
+function setVisibility(main, birdsComponent, birdsEditComponent, mortalityComponent, feedPurchaseComponent, feedConsumptionComponent, feedStockSummaryComoponent, cageComponent,
+                       orderDetailsComponent, placeOrderComponent, profile) {
     document.getElementById('main').style.display = main;
     document.getElementById('birdsComponent').style.display = birdsComponent;
     document.getElementById('birdsEditComponent').style.display = birdsEditComponent;
@@ -46,8 +58,11 @@ function setVisibility(main, birdsComponent, birdsEditComponent, mortalityCompon
     document.getElementById('feedPurchaseComponent').style.display = feedPurchaseComponent;
     document.getElementById('feedConsumptionComponent').style.display = feedConsumptionComponent;
     document.getElementById('feedStockSummaryComoponent').style.display = feedStockSummaryComoponent;
+    document.getElementById('cageComponent').style.display = cageComponent;
+    document.getElementById('orderDetailsComponent').style.display = orderDetailsComponent;
+    document.getElementById('placeOrderComponent').style.display = placeOrderComponent;
     document.getElementById('profile').style.display = profile;
-  //  document.getElementById('footer').style.display = footer;
+    //  document.getElementById('footer').style.display = footer;
 
 
 }
